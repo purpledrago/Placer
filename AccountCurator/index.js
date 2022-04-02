ff16e5408055e671576d03a9d2a140ecc5edc55f
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-04-02 05:47:51 +0400

    Minor improvements and bug fixes across AC

--- AccountCurator/index.js
+++ AccountCurator/index.js
@@ -17,18 +17,39 @@ const port = process.env.PORT || 8080;
 const reddit = simpleOAuth2Reddit.create({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.SECRET_KEY,
-  callbackURL: `http://localhost:${port}/auth/reddit/callback`,
+  callbackURL: `http://localhost:${port}/auth/callback`,
   state: "random-unique-string",
   authorizeOptions: { duration: "permanent" },
 });
 
-app.listen(port, () => console.log(`Served successfully at port ${port}!`));
+fs.open("accounts.json", "r", function (err, fd) {
+  if (err) {
+    fs.writeFile("accounts.json", "{}", function (err) {
+      if (err) {
+        console.log(err);
+      }
+      console.log("accounts.json file created successfully");
+    });
+  }
+});
 
-app.get("/auth/reddit", reddit.authorize);
+app.listen(port, () =>
+  console.log(`Served successfully at port http://localhost:${port}`)
+);
 
-app.get("/auth/reddit/callback", reddit.accessToken, async (req, res) => {
+app.get("/", async (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.send(
+    Buffer.from(
+      "<ul><li><a href='/auth'>Auth</a></li><li><a href='/export'>Export</a></li></ul>"
+    )
+  );
+});
+
+app.get("/auth", reddit.authorize);
+
+app.get("/auth/callback", reddit.accessToken, async (req, res) => {
   let token = req.token.token;
-  res.status(200).json(req.token);
 
   let { data } = await axios({
     url: "https://oauth.reddit.com/api/v1/me",
@@ -46,18 +67,21 @@ app.get("/auth/reddit/callback", reddit.accessToken, async (req, res) => {
   username = data.name;
   id = data.id;
 
-  let rawfile = fs.readFileSync("accounts.json");
-  let accounts = JSON.parse(rawfile);
+  let accounts = JSON.parse(fs.readFileSync("accounts.json"));
 
-  if (!id || id in accounts) {
-    return "Account already exists!";
+  if (!id || accounts.hasOwnProperty(id)) {
+    res.status(409);
+    res.send("Account already exists!");
   } else {
-    accounts.id = { username: username, token: token };
+    accounts[id] = { username: username, token: token };
     fs.writeFileSync("accounts.json", JSON.stringify(accounts));
-    return JSON.stringify(token, null, 2);
+    res.status(200);
+    res.send("Account successfully configured.");
   }
 });
 
 app.get("/export", async (req, res) => {
-  res.sendFile("accounts.json", { root: __dirname });
+  let accounts = JSON.parse(fs.readFileSync("accounts.json"));
+  res.status(200);
+  res.json(accounts);
 });
